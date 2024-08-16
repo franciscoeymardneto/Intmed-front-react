6f2f043788c5c7928fb1ac4c43aaa69a55d3fe0d
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-16 19:22:59 -0300

    feat: implement logo component

---
+++ src/shared/components/Logo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LogoComponent: React.FC = (): JSX.Element => {
+    return (
+        <img src="Logo.png" alt="medicar-logo" />
+    )
+}
+
+export default LogoComponent
