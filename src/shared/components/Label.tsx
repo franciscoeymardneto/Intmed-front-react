b19e2bd58659d44d1278749a7ee9a9a23a3944de
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-16 21:56:51 -0300

    feat: implement label for inputs

---
+++ src/shared/components/Label.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Label = styled.label`
+  font-family: 'Roboto', sans-serif;
+  color: var(--border-color);
+  font-size: 16px;
+  font-weight: normal;
+  position: absolute;
+  pointer-events: none;
+  left: 8px;
+  top: 15px;
+  transition: 0.2s ease all;
+`;
+
+export default Label
