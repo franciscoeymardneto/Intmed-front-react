bc5c986c3060a9fe5b2d9b24f1f787077f537676
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-16 21:56:26 -0300

    feat: implement error span for inputs

---
+++ src/shared/components/ErrorSpan.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const ErrorSpan = styled.span`
+  font-family: 'Roboto', sans-serif;
+  color: var(--error);
+  font-size: 12px;
+  position: absolute;
+  bottom: -20px;
+  left: 5px;
+`;
+
+export default ErrorSpan
