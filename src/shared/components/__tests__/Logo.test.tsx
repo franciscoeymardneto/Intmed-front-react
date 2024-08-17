c222e70fe10277c7fcb2ee187dea37b23b913e3c
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-16 23:13:48 -0300

    test: ensure the behavior of the logo component

---
+++ src/shared/components/__tests__/Logo.test.tsx
@@ -0,0 +1,24 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import Logo from '../Logo';
+import { describe, it, expect } from 'vitest';
+
+describe('Logo', () => {
+  it('should render the logo image', () => {
+    render(<Logo />);
+    const img = screen.getByRole('img');
+    expect(img).toBeInTheDocument();
+  });
+
+  it('should have the correct src attribute', () => {
+    render(<Logo />);
+    const img = screen.getByRole('img');
+    expect(img).toHaveAttribute('src', 'Logo.png');
+  });
+
+  it('should have the correct alt attribute', () => {
+    render(<Logo />);
+    const img = screen.getByRole('img');
+    expect(img).toHaveAttribute('alt', 'medicar-logo');
+  });
+});
