096d3c687c7c5296f2dd94fc07aabdfc667ba9c0
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-18 21:07:15 -0300

    refactor: update auth context with session manegement

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,9 +1,11 @@
 import { createContext, useContext, useState, ReactNode, useEffect } from 'react';
+import { UserSession } from '../core/models/session';
 
 interface AuthContextType {
-  isLoggedIn: boolean;
-  login: () => void;
-  logout: () => void;
+  isLoggedIn: boolean
+  getUserSession: () => UserSession | null
+  setUserSession: (value: UserSession) => void
+  clearSession: () => void
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -19,25 +21,34 @@ export const useAuth = () => {
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
-  const login = () => {
-    localStorage.setItem('isLoggedIn',"true");
-    setIsLoggedIn(true);
-  };
 
-  const logout = () => {
-    localStorage.setItem('isLoggedIn',"false");
-    setIsLoggedIn(false);
-  };
+  function getUserSession(): UserSession | null {
+    const session = localStorage.get('session');
+
+    if (session) {
+      return JSON.parse(session)
+    }
+
+    return null
+  }
+  function setUserSession(value: UserSession):void {
+    localStorage.setItem('session',JSON.stringify(value));
+    setIsLoggedIn(true);
+  }
+  function clearSession(): void {
+    localStorage.removeItem('session')
+    setIsLoggedIn(false)
+  }
 
   useEffect(() => {
-    const savedLoginStatus = localStorage.getItem('isLoggedIn');
-    if (savedLoginStatus === 'true') {
+    const savedLoginStatus = getUserSession();
+    if (savedLoginStatus) {
       setIsLoggedIn(true);
     }
   }, []);
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+    <AuthContext.Provider value={{isLoggedIn, setUserSession, getUserSession, clearSession }}>
       {children}
     </AuthContext.Provider>
   );
