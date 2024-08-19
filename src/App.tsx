9914f0957ccd21879908935e6746335c997b2e1f
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-18 23:52:58 -0300

    chore: add react query provider at app

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,22 @@ import {
 import router from './routes/routes';
 import { AuthProvider } from './contexts/AuthContext';
 import { ToastProvider } from './contexts/ToastContext';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <ToastProvider>
-      <AuthProvider>
-        <RouterProvider router={router} fallbackElement={<h1>LALALALLALALALA</h1>} />
-      </AuthProvider>
-    </ToastProvider>
+    <QueryClientProvider  client={queryClient}>
+      <ToastProvider>
+        <AuthProvider>
+          <RouterProvider router={router} fallbackElement={<h1>LALALALLALALALA</h1>} />
+        </AuthProvider>
+      </ToastProvider>
+    </QueryClientProvider>
   )
 }
 
