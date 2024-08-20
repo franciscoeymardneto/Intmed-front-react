15bad4cdfdb975a97bd67ba82f0a065445aa91fb
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-20 16:46:03 -0300

    feat: implement useSchedule hook to fetch doctors schedules

---
+++ src/hooks/useSchedule.ts
@@ -0,0 +1,40 @@
+import { useQuery } from 'react-query';
+import { useAuth } from '../contexts/AuthContext';
+import { ScheduleService } from '../core/services/schedule-service';
+import { HttpService } from '../core/services/http-service';
+import { ApiError } from '../core/errors/api-error';
+import { useToastStack } from '../contexts/ToastContext';
+import { Schedule } from '../core/models/schedule';
+
+type UseScheduleParams = {
+    doctorId: number
+}
+export const useSchedule = ({
+    doctorId
+}: UseScheduleParams) => {
+    const QueryConsultKey = 'Schedule_QUERY'
+    const pushToast = useToastStack()
+    const authContext = useAuth()
+    const httpService = new HttpService(authContext)
+    const scheduleService = new ScheduleService(httpService);
+
+    const fetchSchedule = useQuery<unknown, ApiError, Schedule[]>({
+        enabled: !!doctorId,
+        queryKey: [QueryConsultKey],
+        queryFn: ({ signal }) => {
+            const source = HttpService.cancelToken()
+            const promise = scheduleService.list(doctorId)
+            signal?.addEventListener('abort', () => source.cancel())
+            return promise
+        },
+        onError: (error) => {
+            pushToast(`Erro ao listar agendas: ${error.message}`, {
+                type: 'error',
+            });
+        },
+    });
+
+    return {
+        fetchSchedule
+    };
+};
